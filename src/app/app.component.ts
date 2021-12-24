c80885e9389561517daade77fe958d2d22daf8d1
Christian MacDonald <39752557+ChristianMacDonald@users.noreply.github.com>
2021-12-24 09:48:50 -0500

    Send employeeList to EmployeesComponent and push new employee to employeeList upon click event from submit button in EmployeeFormComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Employee } from './employee';
+
+import input from '../employees.json';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'se-project';
+  showEmployeeForm = false;
+  employeeList: Array<Employee> = input.employees ? input.employees : [] as Employee[];
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+      console.log(this.employeeList);
+  }
+
+  toggleEmployeeForm(): void {
+    this.showEmployeeForm = !this.showEmployeeForm;
+  }
+
+  addEmployee(e: string): void {
+    console.log(e);
+    try {
+      let employee: Employee = JSON.parse(e);
+      console.log(employee);
+      this.employeeList.push(employee);
+    } catch {
+      console.log('Failed to parse JSON string');
+    }
+  }
 }
