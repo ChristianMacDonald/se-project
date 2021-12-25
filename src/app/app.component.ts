e1ba0f445bbb31efc427173500f2561a5020276c
Christian MacDonald <39752557+ChristianMacDonald@users.noreply.github.com>
2021-12-24 20:52:20 -0500

    Add download button to allow users to download edited employees.json file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Employee } from './employee';
 
 import input from '../employees.json';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -12,12 +13,14 @@ export class AppComponent implements OnInit {
   title = 'se-project';
   showEmployeeForm = false;
   employeeList: Array<Employee> = input.employees ? input.employees : [] as Employee[];
+  downloadJsonHref: SafeUrl;
 
-  constructor() {
+  constructor(private sanitizer: DomSanitizer) {
   }
 
   ngOnInit(): void {
       console.log(this.employeeList);
+      this.generateDownloadJsonUri();
   }
 
   toggleEmployeeForm(): void {
@@ -33,5 +36,12 @@ export class AppComponent implements OnInit {
     } catch {
       console.log('Failed to parse JSON string');
     }
+    this.generateDownloadJsonUri();
+  }
+
+  generateDownloadJsonUri(): void {
+    let data = JSON.stringify({ "employees": this.employeeList });
+    let uri = this.sanitizer.bypassSecurityTrustUrl("data:text/json;charset=UTF-8," + encodeURIComponent(data));
+    this.downloadJsonHref = uri;
   }
 }
